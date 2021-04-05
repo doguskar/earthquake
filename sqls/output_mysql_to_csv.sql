8374a2426e67beb29d9e4b1f15cb477a7466a55e
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2021-04-05 14:07:32 +0300

    update

--- sqls/output_mysql_to_csv.sql
+++ sqls/output_mysql_to_csv.sql
@@ -1,8 +1,13 @@
-SELECT * FROM earthquake.earthquake INTO OUTFILE 'C://ProgramData//MySQL//MySQL Server 8.0//Uploads//earthquake.csv' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+SELECT * FROM earthquake.earthquake INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/earthquake.csv' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+
+
+SELECT * FROM earthquake.earthquake WHERE magnitude >= 4 ORDER BY datetime INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/all_earthquake_magnitude_bigger_than_4_2021_04_05.csv' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 
 SELECT * FROM earthquake.earthquake WHERE lon < 30 AND lon >= 26 AND lat >= 38 AND lat < 40 ORDER BY datetime INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/earthquake_6_2021_04_05.csv' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 
 
+
+SELECT * FROM earthquake.earthquake WHERE lon < 30 AND lon >= 26 AND lat >= 38 AND lat < 40 AND magnitude >= 4 ORDER BY datetime INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/6_earthquake_magnitude_bigger_than_4_2021_04_05.csv' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 
 
 
