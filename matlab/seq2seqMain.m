5a1f8eec5b2597bc661ca2299889fec39bab51e4
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2021-05-21 17:02:00 +0300

    tests

--- matlab/seq2seqMain.m
+++ matlab/seq2seqMain.m
@@ -427,20 +427,36 @@ optMap('wanted_columns') = 7;
 optMap('wanted_columns_str') = "loc";
 optMap('location_label') = 0;
 magnitude_ls = 5;                           %[5 5.3];
-bacward_ls = [20 30 50 100];                %[20 30 50 100];
-batch_ls = 100;                             %[10 20 50 100 100000];
-learn_ls = 0.01;                            %[20 30 50 100];
+bacward_ls = 30;                            %[20 30 50 100];                    30 BEST
+batch_ls = 100;              %[10 20 50 100 100000];             100 BEST               
+learn_ls = 0.05;                            %[0.1 0.01 0.05 0.001 ];        0.01 BEST
 
 for magnitude = 1:size(magnitude_ls,2)
     for bacward = 1:size(bacward_ls,2)
         for batch = 1:size(batch_ls,2)
             for learn = 1:size(learn_ls ,2)
+                
+                backward_size = bacward_ls(bacward);
+                
+                %Column rages
+                magnitude_start     =   9;  
+                magnitude_end       =   magnitude_start     + backward_size + 6;
+                depth_start         =   magnitude_end       + 1;
+                depth_end           =   depth_start         + backward_size + 6;
+                timestamp_start     =   depth_end           + 1;
+                timestamp_end       =   timestamp_start     + backward_size + 6;
+                lat_start           =   timestamp_end       + 1;
+                lat_end             =   lat_start           + backward_size + 6;
+                lon_start           =   lat_end             + 1;
+                lon_end             =   lon_start           + backward_size + 6;
+                
+                
                 optMap('bigger_than_magnitude') = magnitude_ls(magnitude);
                 optMap('backward_size') = bacward_ls(bacward);
                 optMap('mini_batch_size') = batch_ls(batch);
                 optMap('initial_learn_rate') = learn_ls(learn);
 
-                for x=1:1
+                for x=1:4
                     %lat & lon & magnitude & timestamp
                     optMap('feature_columns') = [lat_start:lat_end lon_start:lon_end magnitude_start:magnitude_end timestamp_start:timestamp_end];
                     optMap('feature_columns_str') = "mag_lat_lon_tim";
@@ -464,9 +480,9 @@ optMap('wanted_columns_str') = "mag";
 optMap('location_label') = 6;
 
 magnitude_ls = 5;                           %[4 5 5.3];
-bacward_ls = [20 30 50 100];                %[20 30 50 100];
-batch_ls = 100;                             %[10 20 50 100 100000];
-learn_ls = 0.01;                            %[20 30 50 100];
+bacward_ls = [20 30];                       %[20 30 50 100];           %20 AND 30   20 BETTER 
+batch_ls = 100;                             %[10 20 50 100 100000];          100 BEST
+learn_ls = 0.01;                            %[20 30 50 100];                0.01 BEST
 
 for magnitude = 1:size(magnitude_ls,2)
     for bacward = 1:size(bacward_ls,2)
@@ -476,18 +492,39 @@ for magnitude = 1:size(magnitude_ls,2)
                 optMap('backward_size') = bacward_ls(bacward);
                 optMap('mini_batch_size') = batch_ls(batch);
                 optMap('initial_learn_rate') = learn_ls(learn);
+                
+                
+                
+                
+                
+                backward_size = bacward_ls(bacward);
+                
+                %Column rages
+                magnitude_start     =   9;  
+                magnitude_end       =   magnitude_start     + backward_size + 6;
+                depth_start         =   magnitude_end       + 1;
+                depth_end           =   depth_start         + backward_size + 6;
+                timestamp_start     =   depth_end           + 1;
+                timestamp_end       =   timestamp_start     + backward_size + 6;
+                lat_start           =   timestamp_end       + 1;
+                lat_end             =   lat_start           + backward_size + 6;
+                lon_start           =   lat_end             + 1;
+                lon_end             =   lon_start           + backward_size + 6;
+                
+                
 
-                for x=1:1
+                for x=1:5
+                    %{
                     %magnitude & depth
                     optMap('feature_columns') = [magnitude_start:magnitude_end depth_start:depth_end];
                     optMap('feature_columns_str') = "mag_dep";
                     seq2seqFunc(optMap);
-
+                    %}
                     %magnitude & depth & timestamp
                     optMap('feature_columns') = [magnitude_start:magnitude_end depth_start:depth_end timestamp_start:timestamp_end];
                     optMap('feature_columns_str') = "mag_dep_tim";
                     seq2seqFunc(optMap);
-
+                        
                     %magnitude & lat & lon & depth & timestamp
                     optMap('feature_columns') = [magnitude_start:magnitude_end lat_start:lat_end lon_start:lon_end depth_start:depth_end timestamp_start:timestamp_end];
                     optMap('feature_columns_str') = "mag_lat_lon_dep_tim";
@@ -512,7 +549,7 @@ optMap('wanted_colums_names') = ["Magnitude", "Location Label"];
 optMap('location_label') = 0;
 
 magnitude_ls = 5;                           %[5 5.3];
-bacward_ls = [20 30 50 100];                %[20 30 50 100];
+bacward_ls = [30 40];                       %[20 30 50 100];                % 30 AND 40, 30 BETTER
 batch_ls = 100;                             %[10 20 50 100 100000];
 learn_ls = 0.01;                            %[20 30 50 100];
 
@@ -524,13 +561,32 @@ for magnitude = 1:size(magnitude_ls,2)
                 optMap('backward_size') = bacward_ls(bacward);
                 optMap('mini_batch_size') = batch_ls(batch);
                 optMap('initial_learn_rate') = learn_ls(learn);
+                
+                
+                
+                
+                backward_size = bacward_ls(bacward);
+                
+                %Column rages
+                magnitude_start     =   9;  
+                magnitude_end       =   magnitude_start     + backward_size + 6;
+                depth_start         =   magnitude_end       + 1;
+                depth_end           =   depth_start         + backward_size + 6;
+                timestamp_start     =   depth_end           + 1;
+                timestamp_end       =   timestamp_start     + backward_size + 6;
+                lat_start           =   timestamp_end       + 1;
+                lat_end             =   lat_start           + backward_size + 6;
+                lon_start           =   lat_end             + 1;
+                lon_end             =   lon_start           + backward_size + 6;
+                
 
-                for x=1:1
+                for x=1:5
+                    %{
                     %magnitude
                     optMap('feature_columns') = magnitude_start:magnitude_end;
                     optMap('feature_columns_str') = "mag";
                     seq2seqFunc(optMap);
-
+                    %}
                     %magnitude & depth & timestamp
                     optMap('feature_columns') = [magnitude_start:magnitude_end depth_start:depth_end timestamp_start:timestamp_end];
                     optMap('feature_columns_str') = "mag_dep_tim";
