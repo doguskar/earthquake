4828c6dedbdb50fd09fb2c46d34e56cf6067a3f2
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2021-05-21 20:29:11 +0300

    tests

--- matlab/seq2seqMain.m
+++ matlab/seq2seqMain.m
@@ -6,7 +6,7 @@ keySet = {
 };
 valueSet = {
     "str" ...
-    500 ...
+    1000 ...
     30
 };
 optMap = containers.Map(keySet,valueSet);
@@ -426,10 +426,10 @@ end
 optMap('wanted_columns') = 7;
 optMap('wanted_columns_str') = "loc";
 optMap('location_label') = 0;
-magnitude_ls = 5;                           %[5 5.3];
+magnitude_ls = [5 5.3];                           %[5 5.3];
 bacward_ls = 30;                            %[20 30 50 100];                    30 BEST
-batch_ls = 100;              %[10 20 50 100 100000];             100 BEST               
-learn_ls = 0.05;                            %[0.1 0.01 0.05 0.001 ];        0.01 BEST
+batch_ls = 100;                             %[10 20 50 100 100000];             100 BEST               
+learn_ls = 0.05;                            %[0.1 0.01 0.05 0.001 ];            0.01 BEST
 
 for magnitude = 1:size(magnitude_ls,2)
     for bacward = 1:size(bacward_ls,2)
@@ -456,12 +456,13 @@ for magnitude = 1:size(magnitude_ls,2)
                 optMap('mini_batch_size') = batch_ls(batch);
                 optMap('initial_learn_rate') = learn_ls(learn);
 
-                for x=1:4
+                for x=1:5
+                    %{
                     %lat & lon & magnitude & timestamp
                     optMap('feature_columns') = [lat_start:lat_end lon_start:lon_end magnitude_start:magnitude_end timestamp_start:timestamp_end];
                     optMap('feature_columns_str') = "mag_lat_lon_tim";
                     seq2seqFunc(optMap);
-
+                    %}
                     %lat & lon & magnitude & depth & timestamp
                     optMap('feature_columns') = [lat_start:lat_end lon_start:lon_end magnitude_start:magnitude_end depth_start:depth_end timestamp_start:timestamp_end];
                     optMap('feature_columns_str') = "mag_lat_lon_dep_tim";
@@ -475,66 +476,71 @@ end
 
 
 %% Predict Magnitude
+%{
 optMap('wanted_columns') = 5;
 optMap('wanted_columns_str') = "mag";
-optMap('location_label') = 6;
+%optMap('location_label') = 1:15;
 
-magnitude_ls = 5;                           %[4 5 5.3];
-bacward_ls = [20 30];                       %[20 30 50 100];           %20 AND 30   20 BETTER 
+magnitude_ls = [4.4 5 5.3];                           %[4 5 5.3];
+bacward_ls = 30;                       %[20 30 50 100];                 20 AND 30   20 BETTER 
 batch_ls = 100;                             %[10 20 50 100 100000];          100 BEST
-learn_ls = 0.01;                            %[20 30 50 100];                0.01 BEST
+learn_ls = 0.01;                            %[20 30 50 100];                 0.01 BEST
+location_ls = 0;
 
-for magnitude = 1:size(magnitude_ls,2)
-    for bacward = 1:size(bacward_ls,2)
-        for batch = 1:size(batch_ls,2)
-            for learn = 1:size(learn_ls ,2)
-                optMap('bigger_than_magnitude') = magnitude_ls(magnitude);
-                optMap('backward_size') = bacward_ls(bacward);
-                optMap('mini_batch_size') = batch_ls(batch);
-                optMap('initial_learn_rate') = learn_ls(learn);
-                
-                
-                
-                
-                
-                backward_size = bacward_ls(bacward);
-                
-                %Column rages
-                magnitude_start     =   9;  
-                magnitude_end       =   magnitude_start     + backward_size + 6;
-                depth_start         =   magnitude_end       + 1;
-                depth_end           =   depth_start         + backward_size + 6;
-                timestamp_start     =   depth_end           + 1;
-                timestamp_end       =   timestamp_start     + backward_size + 6;
-                lat_start           =   timestamp_end       + 1;
-                lat_end             =   lat_start           + backward_size + 6;
-                lon_start           =   lat_end             + 1;
-                lon_end             =   lon_start           + backward_size + 6;
-                
-                
+for location = 1:size(location_ls,2)
+    for magnitude = 1:size(magnitude_ls,2)
+        for bacward = 1:size(bacward_ls,2)
+            for batch = 1:size(batch_ls,2)
+                for learn = 1:size(learn_ls ,2)
+                    optMap('bigger_than_magnitude') = magnitude_ls(magnitude);
+                    optMap('backward_size') = bacward_ls(bacward);
+                    optMap('mini_batch_size') = batch_ls(batch);
+                    optMap('initial_learn_rate') = learn_ls(learn);
+                    optMap('location_label') = location_ls(location);
 
-                for x=1:5
-                    %{
-                    %magnitude & depth
-                    optMap('feature_columns') = [magnitude_start:magnitude_end depth_start:depth_end];
-                    optMap('feature_columns_str') = "mag_dep";
-                    seq2seqFunc(optMap);
-                    %}
-                    %magnitude & depth & timestamp
-                    optMap('feature_columns') = [magnitude_start:magnitude_end depth_start:depth_end timestamp_start:timestamp_end];
-                    optMap('feature_columns_str') = "mag_dep_tim";
-                    seq2seqFunc(optMap);
-                        
-                    %magnitude & lat & lon & depth & timestamp
-                    optMap('feature_columns') = [magnitude_start:magnitude_end lat_start:lat_end lon_start:lon_end depth_start:depth_end timestamp_start:timestamp_end];
-                    optMap('feature_columns_str') = "mag_lat_lon_dep_tim";
-                    seq2seqFunc(optMap);
+
+
+
+                    backward_size = bacward_ls(bacward);
+
+                    %Column rages
+                    magnitude_start     =   9;  
+                    magnitude_end       =   magnitude_start     + backward_size + 6;
+                    depth_start         =   magnitude_end       + 1;
+                    depth_end           =   depth_start         + backward_size + 6;
+                    timestamp_start     =   depth_end           + 1;
+                    timestamp_end       =   timestamp_start     + backward_size + 6;
+                    lat_start           =   timestamp_end       + 1;
+                    lat_end             =   lat_start           + backward_size + 6;
+                    lon_start           =   lat_end             + 1;
+                    lon_end             =   lon_start           + backward_size + 6;
+
+
+
+                    for x=1:5
+                        %{
+                        %magnitude & depth
+                        optMap('feature_columns') = [magnitude_start:magnitude_end depth_start:depth_end];
+                        optMap('feature_columns_str') = "mag_dep";
+                        seq2seqFunc(optMap);
+                        %}
+                        %magnitude & depth & timestamp
+                        optMap('feature_columns') = [magnitude_start:magnitude_end depth_start:depth_end timestamp_start:timestamp_end];
+                        optMap('feature_columns_str') = "mag_dep_tim";
+                        seq2seqFunc(optMap);
+                         %{   
+                        %magnitude & lat & lon & depth & timestamp
+                        optMap('feature_columns') = [magnitude_start:magnitude_end lat_start:lat_end lon_start:lon_end depth_start:depth_end timestamp_start:timestamp_end];
+                        optMap('feature_columns_str') = "mag_lat_lon_dep_tim";
+                        seq2seqFunc(optMap);
+                        %}
+                    end
                 end
             end
         end
     end
 end
-%{%}
+%}
 %% Predict Magnitude and Location label
 %{
 
